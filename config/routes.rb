9e56817a86e60d72c69c16dd958bd7068fd9da46
Winnie Zhao <winniemaizhao@gmail.com>
2022-10-19 19:09:27 -0500

    delete extraneous routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,10 @@ Rails.application.routes.draw do
     get "/requests/:id/edit", to: "requests#edit_offering_request", as: :edit_offering_request
     patch "/requests/:id", to: "requests#update_offering_request", as: :update_offering_request
   end
-
   get "/my-requests", to: "requests#my_requests", as: :my_requests
   get "/my-requests/:id", to: "requests#show", as: :my_request
   patch "/my-requests/:id", to: "requests#update"
   delete "/my-requests/:id", to: "requests#destroy"
   get "/my-requests/:id/edit", to: "requests#edit", as: :edit_my_request
-
   get "/my-offerings", to: "equipment#my_offerings", as: :my_offerings
-
-  # get "/my-offering-requests", to: "requests#my_offering_requests", as: :my_offering_requests
-  # get "/my-offering-requests/:id/edit", to: "requests#edit_offering_request", as: :edit_my_offering_request
-  # patch "my-offering-requests/:id", to: "requests#update_offering_request", as: :update_my_offering_request
-
-
 end
